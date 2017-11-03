0c61a26e2f5194e7b8039cfcc6bddf49bcd2629a
MATLAB6699DZ <33332126+MATLAB6699DZ@users.noreply.github.com>
2017-11-03 16:26:40 -0400

    Controls. It works

--- IdahoTrail.m
+++ IdahoTrail.m
@@ -8,44 +8,45 @@ load Adventure
 running = 1;
 posX = 1;
 posY = 1;
-
+World{1,1}= Player;
 imshow([World{1,:};World{2,:};World{3,:};World{4,:};World{5,:};World{6,:};World{7,:};World{8,:};World{9,:};World{10,:};]);
-
-
-
-while running == 1
-    h = figure(1);
-    waitforbuttonpress;
+%Its a for loop right now, not sure what we will all do yet  for the loop so I put a for to test it.
+for k= 1:4
+h = figure(1);
+waitforbuttonpress;
+switch get(h, 'CurrentKey')
+    case 'rightarrow'
+        World{posX,posY} = Blank;
+        posY = posY + 1;
+        World{posX,posY} = Player;
+        imshow([World{1,:};World{2,:};World{3,:};World{4,:};World{5,:};World{6,:};World{7,:};World{8,:};World{9,:};World{10,:};]);
+    case 'downarrow'
+        World{posX,posY} = Blank;
+        posX = posX + 1;
+        World{posX,posY} = Player;
+        imshow([World{1,:};World{2,:};World{3,:};World{4,:};World{5,:};World{6,:};World{7,:};World{8,:};World{9,:};World{10,:};]);
+    case 'uparrow'
+        World{posX,posY} = Blank;
+        posX = posX - 1;
+        World{posX,posY} = Player;
+        imshow([World{1,:};World{2,:};World{3,:};World{4,:};World{5,:};World{6,:};World{7,:};World{8,:};World{9,:};World{10,:};]);
+    case 'leftarrow'
+        World{posX,posY} = Blank;
+        posY = posY - 1;
+        World{posX,posY} = Player;
+        imshow([World{1,:};World{2,:};World{3,:};World{4,:};World{5,:};World{6,:};World{7,:};World{8,:};World{9,:};World{10,:};]);
     
-    %W or up arrow
-    if get(h, 'CurrentKey') == 'downarrow'
-        World{posY, posX} = Blank;
-        posY = posY + 1;
-        World{posY, posX} = Player;
-    end
-    %A or left arraw
-    if get(h, 'CurrentKey') == 'downarrow'
-        World{posY, posX} = Blank;
-        posY = posY + 1;
-        World{posY, posX} = Player;
-    end
-    %D or right awwaor
-    if get(h, 'CurrentKey') == 'downarrow'
-        World{posY, posX} = Blank;
-        posY = posY + 1;
-        World{posY, posX} = Player;
-    end
-    %S or down arow
-    if get(h, 'CurrentKey') == 'downarrow'
-        World{posY, posX} = Blank;
-        posY = posY + 1;
-        World{posY, posX} = Player;
-    end 
-    %SPACE to attac
+    
+    
+end
+end
+% <- arrow
+
+
     %ESCAPE to end game/return to home menu
     
     
     %move enemies
     imshow([World{1,:};World{2,:};World{3,:};World{4,:};World{5,:};World{6,:};World{7,:};World{8,:};World{9,:};World{10,:};]);
 
-end
+
